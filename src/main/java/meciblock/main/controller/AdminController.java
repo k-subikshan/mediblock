7ef2d8c5019709963ec5cf3e0ff0cfb34761a2f4
subikshan <k.subikshandevp@gmail.com>
2026-09-08 22:25:12 +0530

    added doctor ui "

--- src/main/java/meciblock/main/controller/AdminController.java
+++ src/main/java/meciblock/main/controller/AdminController.java
@@ -4,11 +4,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller 
+@RequestMapping("/admin")
 public class AdminController {
-    @GetMapping("/admin")
+    @GetMapping("/dashboard")
     public String getMethodName(Authentication authenticator,Model model) {
         model.addAttribute("username", authenticator.getName());
         return "admin/dashboard";
